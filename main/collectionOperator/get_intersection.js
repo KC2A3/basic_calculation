748098d30fb6e796ce5254febb00e9eff4aded92
KC2A3 <947496351@qq.com>
2018-12-14 15:39:33 +0800

    Get the intersection of two sets

--- main/collectionOperator/get_intersection.js
+++ main/collectionOperator/get_intersection.js
@@ -1,7 +1,15 @@
 'use strict';
 
 function get_intersection(collection_a, collection_b) {
-  //在这里写入代码
+  var result = [];
+  for (var i = 0; i < collection_b.length; i++) {
+    var element = collection_b[i];
+    for (var j = 0; j < collection_a.length; j++) {
+      if (element === collection_a[j]) {
+        result.push(element);
+      }
+    }
+  }
+  return result;
 }
-
 module.exports = get_intersection;
