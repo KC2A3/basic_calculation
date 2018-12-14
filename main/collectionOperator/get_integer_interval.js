0ad847508f93e1d4543515a65482d5a5baf729e5
KC2A3 <947496351@qq.com>
2018-12-14 15:32:09 +0800

    Get a self-increasing number interval based on the two numbers given

--- main/collectionOperator/get_integer_interval.js
+++ main/collectionOperator/get_integer_interval.js
@@ -1,8 +1,20 @@
 'use strict';
 
 function get_integer_interval(number_a, number_b) {
-  //在这里写入代码
+  let x = number_a;
+  let y = number_b;
+  let array = [];
+  if (x < y) {
+    for (let i = x; i <= y; i++) {
+      array.push(i);
+    }
+  } else if (x > y) {
+    for (let i = x+1; i > y, --i;) {
+      array.push(i);
+    }
+  } else if (x === y) {
+    array.push(x);
+  }
+  return array;
 }
-
 module.exports = get_integer_interval;
-
