6bf0736c21b89db6bb635427277d01f07478088f
KC2A3 <947496351@qq.com>
2018-12-17 02:45:20 +0800

    The same element in the A set as the elements of the subarray in the B set

--- main/section_1/practice_2/practice.js
+++ main/section_1/practice_2/practice.js
@@ -1,5 +1,11 @@
 function collect_same_elements(collection_a, collection_b) {
-  //在这里写入代码
+  let oneDimensional = [].concat.apply([], collection_b)
+  let result = [];
+  for (let number of collection_a) {
+    if (oneDimensional.indexOf(number) >= 0) {
+      result.push(number)
+    }
+  }
+  return result;
 }
-
 module.exports = collect_same_elements;
