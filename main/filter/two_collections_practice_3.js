2a206bc00785c6f1129a24cbb006758e6d2dbf24
KC2A3 <947496351@qq.com>
2018-12-16 16:40:35 +0800

    Select the number of A sets that can be divisible by the B set

--- main/filter/two_collections_practice_3.js
+++ main/filter/two_collections_practice_3.js
@@ -1,8 +1,14 @@
 'use strict';
 
 function choose_divisible_integer(collection_a, collection_b) {
-
-  //在这里写入代码
+  let result = [];
+  for (let elementA of collection_a) {
+    for (let elementB of collection_b) {
+      if (elementA % elementB === 0) {
+        result.push(elementA);
+      }
+    }
+  }
+  return result;
 }
-
 module.exports = choose_divisible_integer;
