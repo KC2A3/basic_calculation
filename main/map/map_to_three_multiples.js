eb21f395321d62cd90fe84591355a05b335adca5
KC2A3 <947496351@qq.com>
2018-12-16 17:51:27 +0800

    Elements in set A are mapped to elements in set B，multiply 3

--- main/map/map_to_three_multiples.js
+++ main/map/map_to_three_multiples.js
@@ -1,6 +1,9 @@
 'use strict';
-var map_to_three_multiples = function(collections){
-  return [3,9,15,12,27];
+var map_to_three_multiples = function(collections) {
+  let result = [];
+  for (let element of collections) {
+    result.push(element * 3);
+  }
+  return result;
 };
-
 module.exports = map_to_three_multiples;
