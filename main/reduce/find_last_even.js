d203cdc643746d17ac74600fec57b94e2db2e351
KC2A3 <947496351@qq.com>
2018-12-17 02:41:18 +0800

    The last even number of elements in the collection

--- main/reduce/find_last_even.js
+++ main/reduce/find_last_even.js
@@ -1,7 +1,10 @@
 'use strict';
 
 function find_last_even(collection) {
-  //在这里写入代码
+  let i = collection.length;
+  while (collection[i] % 2 != 0) {
+    i--;
+  }
+  return collection[i];
 }
-
 module.exports = find_last_even;
