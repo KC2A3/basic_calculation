1a3fca425e5b84f06b28371e8865ebcb9332bf36
KC2A3 <947496351@qq.com>
2018-12-17 02:35:47 +0800

    Collect max number

--- main/reduce/collect_max_number.js
+++ main/reduce/collect_max_number.js
@@ -1,7 +1,16 @@
 'use strict';
 
 function collect_max_number(collection) {
-  //在这里写入代码
-}
-
+  let max = [];
+  for (let i = 0; i < collection.length; i++) {
+    for (let j = i; j < collection.length; j++) {
+      if (collection[i] < collection[j]) {
+        max = collection[j];
+        collection[j] = collection[i];
+        collection[i] = max;
+      }
+    }
+  }
+  return collection[0];
+};
 module.exports = collect_max_number;
