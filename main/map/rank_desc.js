cc936ce4683304e563285134a3771aa81f434870
KC2A3 <947496351@qq.com>
2018-12-16 17:53:12 +0800

    Sort from small to large

--- main/map/rank_desc.js
+++ main/map/rank_desc.js
@@ -1,6 +1,16 @@
 'use strict';
 var rank_desc = function(collection){
-  return [2,3,4,5,6];
+  let min = [];
+  for (let i = 0; i < collection.length; i++) {
+    for (let j = i; j < collection.length; j++) {
+      if (collection[i] > collection[j]) {
+        min = collection[j];
+        collection[j] = collection[i];
+        collection[i] = min;
+      }
+    }
+  }
+  return collection;
 };
 
 module.exports = rank_desc;
