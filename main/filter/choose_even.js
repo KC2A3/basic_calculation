c6ad5871a433576519dbd9d11ed3b1ed610a024a
KC2A3 <947496351@qq.com>
2018-12-16 16:13:41 +0800

    Select even number

--- main/filter/choose_even.js
+++ main/filter/choose_even.js
@@ -1,8 +1,12 @@
 'use strict';
 
 function choose_even(collection) {
-
-  //在这里写入代码
+  let result = [];
+  for (var i = 0; i < collection.length; i++) {
+    if (collection[i] % 2 === 0) {
+      result.push(collection[i])
+    }
+  }
+  return result;
 }
-
 module.exports = choose_even;
