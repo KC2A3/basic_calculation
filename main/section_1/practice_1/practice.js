f7e5407978185ba7b18befd276f6c7dea4a0a4ea
KC2A3 <947496351@qq.com>
2018-12-17 02:44:34 +0800

    The same elements in the A set as in the B set

--- main/section_1/practice_1/practice.js
+++ main/section_1/practice_1/practice.js
@@ -1,5 +1,10 @@
 function collect_same_elements(collection_a, collection_b) {
-  //在这里写入代码
+  let result = [];
+  for (let number of collection_a) {
+    if (collection_b.indexOf(number) >= 0) {
+      result.push(number)
+    }
+  }
+  return result;
 }
-
 module.exports = collect_same_elements;
