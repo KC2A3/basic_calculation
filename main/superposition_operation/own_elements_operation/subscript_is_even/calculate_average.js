e8b200a2998086171dda82ce85d4bf269fcbf0b9
KC2A3 <947496351@qq.com>
2018-12-17 22:01:03 +0800

    Calculate the average of the even number of elements

--- main/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
@@ -1,5 +1,13 @@
 'use strict';
-var calculate_average = function(collection){
-
+var calculate_average = function(collection) {
+  let num = 0;
+  let count = 0;
+  for (i = 0; i < collection.length; i++) {
+    if (collection.indexOf(collection[i]) % 2 != 0) {
+      count = count + collection[i];
+      num++;
+    }
+  }
+  return Math.ceil(count / num);
 };
 module.exports = calculate_average;
