bdca0dd406d222c5a7deac1f4a8622ad31f01894
KC2A3 <947496351@qq.com>
2018-12-17 21:59:18 +0800

    Odd arithmetic average of 1-10

--- main/superposition_operation/interval_operation/average_uneven.js
+++ main/superposition_operation/interval_operation/average_uneven.js
@@ -1,8 +1,14 @@
 'use strict';
 
 function average_uneven(collection) {
-
-  //在这里写入代码
+  let count = 0;
+  let num = 0;
+  for (let i = 0; i < collection.length; i++) {
+    if (collection[i] % 2 != 0) {
+      count = count + collection[i];
+      num++;
+    }
+  }
+  return Math.round(count / num);
 }
-
 module.exports = average_uneven;
