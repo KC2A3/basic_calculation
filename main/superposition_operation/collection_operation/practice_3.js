53844f55c4a1faafe2f67d50142e6e7758b46511
KC2A3 <947496351@qq.com>
2018-12-17 21:58:39 +0800

    The sum of each odd number *3+5

--- main/superposition_operation/collection_operation/practice_3.js
+++ main/superposition_operation/collection_operation/practice_3.js
@@ -1,9 +1,13 @@
 'use strict';
 
 function hybrid_operation_to_uneven(collection) {
-
-  //在这里写入代码
+  let count = 0;
+  for (let i = 0; i < collection.length; i++) {
+    if (collection[i] % 2 != 0) {
+      collection[i] = collection[i] * 3 + 5;
+      count = count + collection[i];
+    }
+  }
+  return count;
 }
-
 module.exports = hybrid_operation_to_uneven;
-
