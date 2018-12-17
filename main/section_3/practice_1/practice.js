024cdb934fac2eff33b5c06a56d170480b32ed78
KC2A3 <947496351@qq.com>
2018-12-17 21:53:05 +0800

    The key attribute has the same elements as the elements in the value attribute, and their count-1

--- main/section_3/practice_1/practice.js
+++ main/section_3/practice_1/practice.js
@@ -1,5 +1,12 @@
 function create_updated_collection(collection_a, object_b) {
-  //在这里写入代码
+  let result = [];
+  for (let element of object_b.value) {
+    for (let key of collection_a) {
+      if (key['key'] === element) {
+        key['count'] -= 1
+      }
+    }
+  }
+  return collection_a;
 }
-
 module.exports = create_updated_collection;
