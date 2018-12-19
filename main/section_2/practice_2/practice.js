1447c7bc6b38bdcfe9fba52d868c1ebe1ee49385
KC2A3 <947496351@qq.com>
2018-12-20 02:04:44 +0800

    Count the number of identical elements

--- main/section_2/practice_2/practice.js
+++ main/section_2/practice_2/practice.js
@@ -1,5 +1,30 @@
 function count_same_elements(collection) {
-  //在这里写入代码
+  let result = [];
+  let arr = [];
+  for (let i = 0; i < collection.length; i++) {
+    let element = collection[i].split('-');
+    if (element[1]) {
+      result.push({
+        key: element[0],
+        count: Number(element[1])
+      });
+    } else {
+      let count = 0;
+      for (let j = i; j < collection.length; j++) {
+        if (collection[i] === collection[j]) {
+          count++;
+        }
+      }
+      arr.push({
+        key: collection[i],
+        count: count
+      })
+      i += count;
+    }
+  }
+  for (let k = 0; k < arr.length; k++) {
+    result.push(arr[k]);
+  }
+  return result;
 }
-
 module.exports = count_same_elements;
