b2e4f57c72ea12e993790e3865aee7ae531729ac
KC2A3 <947496351@qq.com>
2018-12-20 02:01:50 +0800

    Get the letter range from the given number

--- main/collectionOperator/get_letter_interval_2.js
+++ main/collectionOperator/get_letter_interval_2.js
@@ -1,8 +1,9 @@
 'use strict';
+var number_map_to_word_over_26 = require('../map/number_map_to_word_over_26.js');
+var get_integer_interval = require('./get_integer_interval.js')
 
 function get_letter_interval_2(number_a, number_b) {
-  //在这里写入代码
+  let newarry = get_integer_interval(number_a, number_b);
+  return number_map_to_word_over_26(newarry);
 }
-
 module.exports = get_letter_interval_2;
-
