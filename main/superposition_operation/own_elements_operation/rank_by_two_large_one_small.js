851b3a7498a7491290385af5f9ed8cd1fbaf4e0b
KC2A3 <947496351@qq.com>
2018-12-20 02:40:04 +0800

    Optimization code

--- main/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
+++ main/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
@@ -1,19 +1,33 @@
 'use strict';
 
 function rank_by_two_large_one_small(collection) {
+  //sort
+  let afterRank = rankAsc(collection);
   let result = [];
-  let newarry = collection.sort(smallToBig);
-  let num = parseInt(newarry / newarry.length);
-  for (i = 0; i < num; i++) {
-    let smallest = collection.splice(i * 3, 1);
-    collection.splice(i * 3 + 2, 0, smallest[0]);
+  for (let i = 0; i <= Math.floor(afterRank.length / 3); i++) {
+    let begin = i * 3;
+    let end = begin + 3;
+    let sliceArr = end <= afterRank.length ? afterRank.slice(begin, end) : afterRank.slice(begin);
+    result = sliceArr.length === 3 ? result.concat(twoLargeBeforeOneSmall(sliceArr)) : result.concat(sliceArr);
   }
-  return collection;
-  }
+  return result;
+}
 
-  function smallToBig(a, b) {
-    if (a > b) {
-      return 1;
+function twoLargeBeforeOneSmall(collection) {
+  collection.push(collection.shift());
+  return collection;
+}
+
+function rankAsc(collection) {
+  for (let i = collection.length; i >= 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (collection[j] > collection[j + 1]) {
+        let min = collection[j];
+        collection[j] = collection[j + 1];
+        collection[j + 1] = min;
+      }
     }
   }
-  module.exports = rank_by_two_large_one_small;
+  return collection;
+}
+module.exports = rank_by_two_large_one_small;
