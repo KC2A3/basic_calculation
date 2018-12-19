be013819b220697db8e80fe71459ee94107f569c
KC2A3 <947496351@qq.com>
2018-12-20 02:01:19 +0800

    Optimization code

--- main/collectionOperator/get_integer_interval.js
+++ main/collectionOperator/get_integer_interval.js
@@ -9,7 +9,7 @@ function get_integer_interval(number_a, number_b) {
       array.push(i);
     }
   } else if (x > y) {
-    for (let i = x+1; i > y, --i;) {
+    for (let i = x; i >= y; --i) {
       array.push(i);
     }
   } else if (x === y) {
