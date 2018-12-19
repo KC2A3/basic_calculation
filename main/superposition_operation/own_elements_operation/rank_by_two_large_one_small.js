1e7e3b8b07bb8fbae9c9ddd37059e134cbfa3d44
KC2A3 <947496351@qq.com>
2018-12-20 02:39:27 +0800

    Rank by two large one small

--- main/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
+++ main/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
@@ -1,5 +1,19 @@
 'use strict';
-function rank_by_two_large_one_small(collection){
-  //这里写代码。。。
-}
-module.exports = rank_by_two_large_one_small;
+
+function rank_by_two_large_one_small(collection) {
+  let result = [];
+  let newarry = collection.sort(smallToBig);
+  let num = parseInt(newarry / newarry.length);
+  for (i = 0; i < num; i++) {
+    let smallest = collection.splice(i * 3, 1);
+    collection.splice(i * 3 + 2, 0, smallest[0]);
+  }
+  return collection;
+  }
+
+  function smallToBig(a, b) {
+    if (a > b) {
+      return 1;
+    }
+  }
+  module.exports = rank_by_two_large_one_small;
